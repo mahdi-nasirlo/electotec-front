3bd509905582c8f753f48b22eb22626ec7ff47eb
mahdi <m.nwordpress1223@gmail.com>
2023-11-20 15:09:07 +0330

    ADD:: create insert form controller

--- components/client-component/AccountCreateContractClient/ContractInsert.tsx
+++ components/client-component/AccountCreateContractClient/ContractInsert.tsx
@@ -1,10 +1,85 @@
 import React from 'react';
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import useControlContract
+    from "@/components/client-component/AccountCreateContractClient/state-managment/useControlContract";
+import {cn} from "@/lib/utils";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import Empty from "@/components/ui/empty";
 
 const ContractInsert = () => {
+
+    const controlContract = useControlContract()
+
+    const {form, fieldArray} = controlContract.insert
+
+    function onSubmit(data: any) {
+
+        console.log(data)
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <div>
+                    {fieldArray.fields.map((field, index) => (
+                        <FormField
+                            control={form.control}
+                            key={field.id}
+                            name={`values.${index}.name`}
+                            render={({field: renderField}) => (
+                                <FormItem>
+                                    <FormLabel className={cn(index !== 0 && "sr-only")}>
+                                        مقادیر متغیر
+                                    </FormLabel>
+                                    <FormDescription
+                                        style={{direction: "rtl"}}
+                                        className={cn(index !== 0 && "sr-only")}
+                                    >
+                                        در این قسمت میتوانید مقادیر متغیر یا مقادیری که نیاز به ورود اطلاعات دارند
+                                        را
+                                        مشخص کنید.
+                                    </FormDescription>
+                                    <FormControl>
+                                        <Input {...renderField} defaultValue={field.name}/>
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                    ))}
+                    {fieldArray.fields.length === 0 ? <>
+                        <div className="flex items-center justify-center flex-col">
+                            <Empty className="w-full h-44" title="مقداری وجود ندارد"/>
+                            <Button
+                                className="mt-3"
+                                variant="secondary"
+                                size="sm"
+                                onClick={() => fieldArray.append({name: ""})}
+                            >
+                                افزودن متغیر
+                            </Button>
+                        </div>
+                    </> : <div className="flex justify-start gap-3">
+                        <Button
+                            type="button"
+                            variant="secondary"
+                            className="mt-3"
+                            onClick={() => fieldArray.append({name: ""})}
+                        >
+                            افزودن متغیر
+                        </Button>
+                        <Button
+                            type="submit"
+                            variant="default"
+                            className="mt-3"
+                        >
+                            ثبت
+                        </Button>
+                    </div>}
+                </div>
+            </form>
+        </Form>
     );
 };
 
--- components/client-component/AccountCreateContractClient/state-managment/ControlContractProvider.tsx
+++ components/client-component/AccountCreateContractClient/state-managment/ControlContractProvider.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import {useForm, UseFormReturn} from "react-hook-form";
+import {useFieldArray, UseFieldArrayReturn, useForm, UseFormReturn} from "react-hook-form";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 
 interface ControlContractType {
     contentForm: UseFormReturn<{ content: string; }, any, undefined>,
     contentFormSchema: z.ZodObject<{ content: z.ZodString }>,
-    insertForm: UseFormReturn<any>,
-    insertFormSchema: z.ZodObject<any>
+    insert: {
+        form: UseFormReturn<any>,
+        schema: z.ZodObject<any>,
+        fieldArray: UseFieldArrayReturn<{ values?: { name: string }[] | undefined }>
+    }
 }
+
+const contentFormSchema = z.object({
+    content: z.string().min(24, {message: "حداقل طول متن قرارداد ۲۴ کاراکتر است"})
+}).required()
+
+
+const insertFormSchema = z.object({
+    values: z.array(
+        z.object({
+            name: z.string().min(3, {message: "لطفا مقدار را وارد کنید"}),
+        }).required()
+    ).optional(),
+})
+
 
 export const ControlContractProvider = (props: { children: React.ReactNode }) => {
 
@@ -20,24 +37,26 @@ export const ControlContractProvider = (props: { children: React.ReactNode }) =>
     })
 
     const insertForm = useForm<z.infer<typeof insertFormSchema>>({
-        resolver: zodResolver(contentFormSchema),
+        resolver: zodResolver(insertFormSchema),
+        mode: "onChange"
+    })
+
+    const fieldArray = useFieldArray({
+        name: "values",
+        control: insertForm.control,
     })
 
     return <ControlContractContext.Provider
         value={{
             contentForm,
             contentFormSchema,
-            insertForm,
-            insertFormSchema
+            insert: {
+                form: insertForm,
+                schema: insertFormSchema,
+                fieldArray
+            }
         }}>{props.children}</ControlContractContext.Provider>
 }
-
-const contentFormSchema = z.object({
-    content: z.string().min(24, {message: "حداقل طول متن قرارداد ۲۴ کاراکتر است"})
-}).required()
-
-
-const insertFormSchema = z.object({})
 
 
 export const ControlContractContext = React.createContext<ControlContractType>({} as ControlContractType)
